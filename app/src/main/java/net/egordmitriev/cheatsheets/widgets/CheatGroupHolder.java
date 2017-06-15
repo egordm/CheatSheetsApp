f58e2d418f49c071c0c11aa3f792665b95caafd4
EgorDm <egordmitriev2@gmail.com>
2017-06-15 11:46:16 +0200

    Added constructor

---
+++ app/src/main/java/net/egordmitriev/cheatsheets/widgets/CheatGroupHolder.java
@@ -0,0 +1,15 @@
+package net.egordmitriev.cheatsheets.widgets;
+
+import android.view.View;
+
+import net.egordmitriev.cheatsheets.pojo.CheatGroup;
+
+/**
+ * Created by EgorDm on 14-Jun-2017.
+ */
+
+public class CheatGroupHolder extends ViewHolder<CheatGroup> {
+    public CheatGroupHolder(View view) {
+        super(view);
+    }
+}
