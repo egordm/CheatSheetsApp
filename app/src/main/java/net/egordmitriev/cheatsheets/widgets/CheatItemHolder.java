dc668e30a5ff74ae62c02b48946e8115b9fb71d1
EgorDm <egordmitriev2@gmail.com>
2017-06-18 00:42:43 +0200

    Added columns & desc format TODO:  second row for desc

--- app/src/main/java/net/egordmitriev/cheatsheets/widgets/CheatItemHolder.java
+++ app/src/main/java/net/egordmitriev/cheatsheets/widgets/CheatItemHolder.java
@@ -1,10 +1,14 @@
 package net.egordmitriev.cheatsheets.widgets;
 
 import android.app.Activity;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import com.orhanobut.logger.Logger;
 
 import net.egordmitriev.cheatsheets.R;
 import net.egordmitriev.cheatsheets.pojo.Cheat;
@@ -25,8 +29,16 @@ public class CheatItemHolder extends ViewHolder<Cheat> {
 
     protected Activity mActivity;
 
-    @BindView(R.id.title)
-    TextView mTitleView;
+/*
+    @BindView(R.id.expandable_contents)
+    TableLayout mTableLayout;
+*/
+
+    @BindView(R.id.content_left)
+    TextView mContentLeft;
+
+    @BindView(R.id.content_right)
+    TextView mContentRight;
 
     public CheatItemHolder(Activity activity, View view) {
         super(view);
@@ -37,17 +49,32 @@ public class CheatItemHolder extends ViewHolder<Cheat> {
     public void onBind(Cheat data, int position) {
         onBind(data);
 
-        //Workaround padding
-        int padding = 12; // in pixels
-        mTitleView.setShadowLayer(padding /* radius */, 0, 0, 0 /* transparent */);
-        mTitleView.setPadding(padding, padding, padding, padding);
+        applyWorkaround(mContentLeft);
+        applyWorkaround(mContentRight);
 
         try {
-            mTitleView.setText(CodeSpannableBuilder.fromHtml(data.content.get(0)));
+            mContentLeft.setText(CodeSpannableBuilder.fromHtml(data.content.get(0)));
+            if (data.layout == 0 && data.content.size() > 1) {
+                mContentRight.setText(CodeSpannableBuilder.fromHtml(data.content.get(1)));
+            } else if (data.layout == 1 && !TextUtils.isEmpty(data.description)) {
+                mContentRight.setText(CodeSpannableBuilder.fromHtml(data.description));
+            } else {
+                mContentRight.setVisibility(View.GONE);
+                TableRow.LayoutParams params = (TableRow.LayoutParams) mContentLeft.getLayoutParams();
+                params.span = 2;
+                mContentLeft.setLayoutParams(params);
+            }
+
         } catch (ParserConfigurationException | SAXException e) {
             e.printStackTrace();
         }
         mView.setBackgroundResource((position % 2 == 0) ? R.color.tableEven : R.color.tableUneven);
+    }
+
+    public void applyWorkaround(TextView textView) {
+        int padding = 12;
+        textView.setShadowLayer(padding /* radius */, 0, 0, 0 /* transparent */);
+        textView.setPadding(padding, padding, padding, padding);
     }
 
     @Override
@@ -56,8 +83,8 @@ public class CheatItemHolder extends ViewHolder<Cheat> {
     }
 
     public static View inflate(LayoutInflater inflater, ViewGroup parent) {
-        View view = inflater.inflate(R.layout.cheat_item, parent, false);
-        parent.addView(view);
-        return view;
+        int tempI = parent.getChildCount();
+        View view = inflater.inflate(R.layout.cheat_item, parent, true);
+        return parent.getChildAt(tempI);
     }
 }
