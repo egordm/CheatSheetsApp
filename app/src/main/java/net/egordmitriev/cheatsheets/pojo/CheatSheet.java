81b3ef162d69ca5d0b2d1559d4be88b8c2fd843d
Egor Dmitriev <egordmitriev2@gmail.com>
2017-08-16 22:46:15 +0200

    Added meta fields to cheatsheet model

--- app/src/main/java/net/egordmitriev/cheatsheets/pojo/CheatSheet.java
+++ app/src/main/java/net/egordmitriev/cheatsheets/pojo/CheatSheet.java
@@ -1,6 +1,7 @@
 package net.egordmitriev.cheatsheets.pojo;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -9,23 +10,27 @@ import java.util.List;
 
 public class CheatSheet extends MatchableModel {
     public int id;
+    public int type;
     public String title;
     public String subtitle;
     public String description;
     public List<CheatGroup> cheat_groups;
     public List<String> tags;
+    public Date last_sync;
     public boolean isLocal;
 
     public CheatSheet() {
     }
-
-    public CheatSheet(int id, String title, String subtitle, String description, List<CheatGroup> cheat_groups, List<String> tags) {
+    
+    public CheatSheet(int id, int type, String title, String subtitle, String description, List<CheatGroup> cheat_groups, List<String> tags, Date last_sync) {
         this.id = id;
+        this.type = type;
         this.title = title;
         this.subtitle = subtitle;
         this.description = description;
         this.cheat_groups = cheat_groups;
         this.tags = tags;
+        this.last_sync = last_sync;
     }
 
     @Override
