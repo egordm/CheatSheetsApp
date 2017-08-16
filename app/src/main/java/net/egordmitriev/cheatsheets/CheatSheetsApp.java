68b30b52a8a49c463de58f2d8f28e6ba091c45d8
Egor Dmitriev <egordmitriev2@gmail.com>
2017-08-16 16:40:59 +0200

    Added creation & closing of registry inside the application TODO: Move it to somewhere else since we dont use it the whole time

--- app/src/main/java/net/egordmitriev/cheatsheets/CheatSheetsApp.java
+++ app/src/main/java/net/egordmitriev/cheatsheets/CheatSheetsApp.java
@@ -8,30 +8,43 @@ import android.net.NetworkInfo;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 
+import net.egordmitriev.cheatsheets.api.Registry;
+
 /**
  * Created by EgorDm on 12-Jun-2017.
  */
 
 public class CheatSheetsApp extends Application {
-
-    private static Context context;
-
-    public void onCreate() {
-        super.onCreate();
-        CheatSheetsApp.context = getApplicationContext();
-        Logger.addLogAdapter(new AndroidLogAdapter());
-    }
-
-    public static Context getAppContext() {
-        return CheatSheetsApp.context;
-    }
-
-
-    public static boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getAppContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
-
+	
+	private static Context sContext;
+	private static Registry sRegistry;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		sContext = getApplicationContext();
+		Logger.addLogAdapter(new AndroidLogAdapter());
+		sRegistry = new Registry(sContext);
+	}
+	
+	public static Context getAppContext() {
+		return sContext;
+	}
+	
+	public static Registry getRegistry() {
+		return sRegistry;
+	}
+	
+	public static boolean isNetworkAvailable() {
+		ConnectivityManager connectivityManager
+				= (ConnectivityManager) getAppContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
+	
+	@Override
+	public void onTerminate() {
+		super.onTerminate();
+		sRegistry.close();
+	}
 }
