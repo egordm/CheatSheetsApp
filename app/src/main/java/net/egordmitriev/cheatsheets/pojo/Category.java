8166a9826ddcfb752c5cfa68551d1fb64d6ac61e
Egor Dmitriev <egordmitriev2@gmail.com>
2017-08-16 16:38:16 +0200

    Added id to category fields

--- app/src/main/java/net/egordmitriev/cheatsheets/pojo/Category.java
+++ app/src/main/java/net/egordmitriev/cheatsheets/pojo/Category.java
@@ -7,50 +7,58 @@ import java.util.List;
  * Created by EgorDm on 12-Jun-2017.
  */
 
-public class Category extends MatchableModel{
-    public boolean temp;
-    public String title;
-    public String description;
-    public List<CheatSheet> cheat_sheets;
-
-    public Category() {
-    }
-
-    public Category(String title, String description, List<CheatSheet> cheat_sheets) {
-        this.title = title;
-        this.description = description;
-        this.cheat_sheets = cheat_sheets;
-    }
-
-    @Override
-    protected List<String> getSearchableStrings(boolean recursive) {
-        List<String> ret = new ArrayList<String>() {{
-            add(title);
-            add(description);
-        }};
-        if(recursive) {
-            for (CheatSheet cheatSheet : cheat_sheets) {
-                ret.addAll(cheatSheet.getSearchableStrings(false));
-            }
-        }
-        return ret;
-    }
-
-    public CheatSheet getCheatSheet(int id) {
-        for(CheatSheet cheatSheet : cheat_sheets) {
-            if(cheatSheet.id == id) {
-                return cheatSheet;
-            }
-        }
-        return null;
-    }
-
-    public static CheatSheet getCheatSheet(int id, List<Category> categories) {
-        for (Category category : categories) {
-            CheatSheet temp = category.getCheatSheet(id);
-            if(temp != null) return temp;
-        }
-        return null;
-    }
-
+public class Category extends MatchableModel {
+	public boolean temp;
+	public int id;
+	public String title;
+	public String description;
+	public List<CheatSheet> cheat_sheets;
+	
+	public Category() {
+	}
+	
+	public Category(int id, String title, String description, List<CheatSheet> cheat_sheets) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.cheat_sheets = cheat_sheets;
+	}
+	
+	public Category(String title, String description, List<CheatSheet> cheat_sheets) {
+		this.title = title;
+		this.description = description;
+		this.cheat_sheets = cheat_sheets;
+	}
+	
+	@Override
+	protected List<String> getSearchableStrings(boolean recursive) {
+		List<String> ret = new ArrayList<String>() {{
+			add(title);
+			add(description);
+		}};
+		if (recursive) {
+			for (CheatSheet cheatSheet : cheat_sheets) {
+				ret.addAll(cheatSheet.getSearchableStrings(false));
+			}
+		}
+		return ret;
+	}
+	
+	public CheatSheet getCheatSheet(int id) {
+		for (CheatSheet cheatSheet : cheat_sheets) {
+			if (cheatSheet.id == id) {
+				return cheatSheet;
+			}
+		}
+		return null;
+	}
+	
+	public static CheatSheet getCheatSheet(int id, List<Category> categories) {
+		for (Category category : categories) {
+			CheatSheet temp = category.getCheatSheet(id);
+			if (temp != null) return temp;
+		}
+		return null;
+	}
+	
 }
